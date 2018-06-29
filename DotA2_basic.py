3df8ecbf1f1683a336bb29e24d9a383822f7c24d
Vaspra <douglawrence0010@gmail.com>
2018-06-29 11:57:41 +0100

    "Image Name" fix

--- DotA2_basic.py
+++ DotA2_basic.py
@@ -32,37 +32,37 @@ TABLE_FILENAME = 'DotA2Table.xlsx'
 
 # Define the data specific function
 def dota_heroes_upload(reporter, client, base_path):
-    
+
     # Initial collection creation
     description = 'A collection of DotA 2 hero information'
     collection = client.create_collection('DotA 2', description)
     reporter('Created collection', level=0)
-    
+
     # Create the imageset
     imageset_id = collection['imageset_id']
     reporter('Uploading images', level=0)
     image_directory = os.path.join(base_path, PORTRAITS_DIRECTORY)
-    
+
     # For each image found in the image folder, upload to the imageset
     for filename in os.listdir(image_directory):
         with open(os.path.join(image_directory, filename), 'rb') as f:
             client.upload_png(imageset_id, filename, f)
     reporter('Uploaded %d pngs to client' % len(os.listdir(image_directory)),
         level=0)
-            
+
     # Create the dataset
     dataset_id = collection['dataset_id']
     with open(os.path.join(base_path, TABLE_FILENAME), 'rb') as f:
         client.upload_data(dataset_id, TABLE_FILENAME, f)
     reporter('Uploaded data from %s to client' % TABLE_FILENAME, level=0)
-    
+
     # Create the join dataset
     join_ds = client.create_join('Join for DotA2 dataset', imageset_id, dataset_id,
-                                 join_field='ImageName')
+                                 join_field='Image Name')
     collection['join_dataset_id'] = join_ds['id']
     client.update_collection(collection['id'], collection)
 
-            
+
 # Define the argument parser
 def parse_args(argv):
     parser = argparse.ArgumentParser(argv[0])
